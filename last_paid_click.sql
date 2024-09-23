c48cc5d1fc2725995cad4f677ed09343bd293ef6
Daniil <128177413+beton18@users.noreply.github.com>
2024-09-23 18:43:04 +0300

    Оптимизация и сокращение запроса

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,67 +1,24 @@
-WITH ad_data AS (
-    SELECT
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        utm_content,
-        campaign_date,
-        daily_spent
-    FROM ya_ads
-    UNION ALL
-    SELECT
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        utm_content,
-        campaign_date,
-        daily_spent
-    FROM vk_ads
-),
-
-sessions_with_ads AS (
-    SELECT
+WITH last_paid_clicks AS (
+    SELECT DISTINCT ON (s.visitor_id)
         s.visitor_id,
         s.visit_date,
+        s.source AS utm_source,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
         l.lead_id,
         l.created_at,
         l.amount,
         l.closing_reason,
-        l.status_id,
-        COALESCE(a.utm_source, s.source) AS utm_source,
-        COALESCE(a.utm_medium, s.medium) AS utm_medium,
-        COALESCE(a.utm_campaign, s.campaign) AS utm_campaign
+        l.status_id
     FROM sessions AS s
-    LEFT JOIN ad_data AS a
-        ON
-            s.source = a.utm_source
-            AND s.medium = a.utm_medium
-            AND s.campaign = a.utm_campaign
-            AND s.visit_date::date = a.campaign_date
     LEFT JOIN leads AS l
-        ON
-            s.visitor_id = l.visitor_id
-            AND s.visit_date <= l.created_at
-),
-
-last_paid_clicks AS (
-    SELECT DISTINCT ON (visitor_id)
-        visitor_id,
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign,
-        lead_id,
-        created_at,
-        amount,
-        closing_reason,
-        status_id
-    FROM sessions_with_ads
-    WHERE utm_medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    ORDER BY visitor_id ASC, visit_date DESC
+        ON s.visitor_id = l.visitor_id
+        AND s.visit_date <= l.created_at
+    WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    ORDER BY s.visitor_id, s.visit_date DESC
 )
 
 SELECT *
 FROM last_paid_clicks
-ORDER BY
-    amount DESC NULLS LAST, visit_date ASC, utm_source ASC, utm_medium ASC, utm_campaign ASC
-    LIMIT 10;
+ORDER BY amount DESC NULLS LAST, visit_date ASC, utm_source ASC, utm_medium ASC, utm_campaign ASC
+LIMIT 10;
