5aecf55523d5b58aa08effb0fc2f4c6a2526678f
Daniil <128177413+beton18@users.noreply.github.com>
2024-09-29 23:36:57 +0300

    Update aggregate_last_paid_click.sql
    линтер

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,66 +1,66 @@
 -- Подзапрос visitors_and_leads получает уникальные 
---визиты пользователей с данными о лидах
-with visitors_and_leads as (
-    select distinct on (s.visitor_id)--убираем дубли
+-- визиты пользователей с данными о лидах
+WITH visitors_and_leads AS (
+    SELECT DISTINCT ON (s.visitor_id) -- убираем дубли
         s.visitor_id,  -- ID посетителя
         s.visit_date,  -- Дата визита
-        s.source as utm_source,  -- UTM-источник
-        s.medium as utm_medium,  -- UTM-тип трафика (cpc, cpm и т.д.)
-        s.campaign as utm_campaign,  -- UTM-кампания
+        s.source AS utm_source,  -- UTM-источник
+        s.medium AS utm_medium,  -- UTM-тип трафика (cpc, cpm и т.д.)
+        s.campaign AS utm_campaign,  -- UTM-кампания
         l.lead_id,  -- ID лида, если есть
         l.amount,  -- Сумма лида
         l.created_at,  -- Дата создания лида
         l.status_id  -- Статус лида (например, покупка)
-    from sessions as s
-    left join leads as l
-        on
+    FROM sessions AS s
+    LEFT JOIN leads AS l
+        ON
             s.visitor_id = l.visitor_id  -- Привязываем лиды к визитам
-            and s.visit_date <= l.created_at
-    where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-    order by 1, 2 desc  -- Сортируем по visitor_id и дате визита (для distinct)
+            AND s.visit_date <= l.created_at
+    WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    ORDER BY 1, 2 DESC  -- Сортируем по visitor_id и дате визита (для distinct)
 ),
 
 -- Подзапрос costs собирает расходы по рекламным кампаниям из двух таблиц
-costs as (
-    select
+costs AS (
+    SELECT
         campaign_date::date,  -- Дата кампании
-        SUM(daily_spent) as daily_spent,  -- Общие расходы за день
+        SUM(daily_spent) AS daily_spent,  -- Общие расходы за день
         utm_source,  -- UTM-источник
         utm_medium,  -- UTM-тип трафика
         utm_campaign  -- UTM-кампания
-    from vk_ads  -- Таблица расходов ВК
-    group by 1, 3, 4, 5--Группируем по дате, источнику, типу трафика и кампании
-    union all
-    select
+    FROM vk_ads  -- Таблица расходов ВК
+    GROUP BY 1, 3, 4, 5 -- Группируем по дате, источнику, типу трафика и кампании
+    UNION ALL
+    SELECT
         campaign_date::date,  -- Аналогично для яндекс-рекламы
-        SUM(daily_spent) as daily_spent,
+        SUM(daily_spent) AS daily_spent,
         utm_source,
         utm_medium,
         utm_campaign
-    from ya_ads  -- Таблица расходов Яндекс
-    group by 1, 3, 4, 5
+    FROM ya_ads  -- Таблица расходов Яндекс
+    GROUP BY 1, 3, 4, 5
 )
 
---Соединяем данные визитов с расходами и считающий метрики
-select
+-- Соединяем данные визитов с расходами и считаем метрики
+SELECT
     vl.visit_date::date,  -- Дата визита
-    COUNT(*) as visitors_count,  -- Количество визитов
     vl.utm_source,  -- UTM-источник
     vl.utm_medium,  -- UTM-тип трафика
     vl.utm_campaign,  -- UTM-кампания
-    daily_spent as total_cost,--Общие расходы на рекламу
-    COUNT(*) filter (where lead_id is not NULL) as leads_count,
-    COUNT(*) filter (where status_id = 142) as purchases_count,
-    COALESCE(SUM(amount) filter (where status_id = 142), 0) as revenue
-from visitors_and_leads as vl
-left join costs as c  -- Левый джойн с таблицей расходов
-    on
+    c.daily_spent AS total_cost, -- Общие расходы на рекламу
+    COUNT(*) AS visitors_count,  -- Количество визитов
+    COUNT(*) FILTER (WHERE vl.lead_id IS NOT NULL) AS leads_count,
+    COUNT(*) FILTER (WHERE vl.status_id = 142) AS purchases_count,
+    COALESCE(SUM(vl.amount) FILTER (WHERE vl.status_id = 142), 0) AS revenue
+FROM visitors_and_leads AS vl
+LEFT JOIN costs AS c  -- Левый джойн с таблицей расходов
+    ON
         vl.utm_source = c.utm_source  -- По UTM-источнику
-        and vl.utm_medium = c.utm_medium  -- По типу трафика
-        and vl.utm_campaign = c.utm_campaign  -- По кампании
-        and vl.visit_date::date = c.campaign_date::date  -- И по дате
-group by 1, 3, 4, 5, 6
---Группируем по дате, источнику, типу трафика, кампании и расходам
-order by 9 desc nulls last, 2 desc, 1, 3, 4, 5
---Сортируем сначала по доходу, затем по количеству визитов
-limit 15; --Лимитируем результат
+        AND vl.utm_medium = c.utm_medium  -- По типу трафика
+        AND vl.utm_campaign = c.utm_campaign  -- По кампании
+        AND vl.visit_date::date = c.campaign_date::date  -- И по дате
+GROUP BY 1, 2, 3, 4, 5
+-- Группируем по дате, источнику, типу трафика, кампании и расходам
+ORDER BY 9 DESC NULLS LAST, 6 DESC, 1, 2, 3, 4
+-- Сортируем сначала по доходу, затем по количеству визитов
+LIMIT 15; -- Лимитируем результат
